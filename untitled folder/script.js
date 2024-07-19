9b5e7d5ec5d275fa595d7d16e621acc204b10347
Sandipto Roy <sandipto729@gmail.com>
2024-07-19 22:19:20 +0530

    add files 1

---
+++ untitled folder/script.js
@@ -0,0 +1,14 @@
+// alert("Hello, World!");
+let menuButton = document.querySelector(".menu-icon");
+let crossIcon = document.querySelector("#cross"); // Directly target the cross icon
+let menu = document.querySelector(".hidden-icon"); // The menu is the parent of the cross icon
+
+menuButton.addEventListener('click', function() {
+    menu.classList.add("class1");
+    menu.classList.remove("hidden-icon");
+});
+
+crossIcon.addEventListener('click', function() {
+    menu.classList.add("hidden-icon");
+    menu.classList.remove("class1");
+});
