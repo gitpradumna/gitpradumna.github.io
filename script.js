7cb6d84f07dba4a12564df0f4fd2475080681bfb
Sandipto Roy <sandipto729@gmail.com>
2024-07-20 10:45:26 +0530

    sliding photo bar add change 4

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ crossIcon.addEventListener('click', function() {
     menu.classList.add("hidden-icon");
     menu.classList.remove("class1");
 });
+
+
+//photo next button and pre button
+let slides = document.querySelectorAll('.section-image');
+let cnt = 0;
+
+slides.forEach((slide, idx) => {
+    slide.style.left = `${idx * 100}%`;
+});
+
+function prevPhoto() {
+    cnt = (cnt > 0) ? cnt - 1 : slides.length - 1;
+    scroll();
+}
+
+function nextPhoto() {
+    cnt = (cnt < slides.length - 1) ? cnt + 1 : 0;
+    scroll();
+}
+
+function scroll() {
+    slides.forEach(slide => {
+        slide.style.transform = `translateX(-${cnt * 100}%)`;
+    });
+}
